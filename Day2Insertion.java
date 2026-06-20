fb9452940ce497452a8b4244400372b340fe01d9
POLOJU ARUN KUMAR <arunkumarpoloju2004@gmail.com>
2026-06-20 19:57:10 +0530

    Update and rename Insertion.java to Day2Insertion.java

--- Day2Insertion.java
+++ Day2Insertion.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
-public class Insertion {
+public class Day2Insertion {
     public static void main(String[] args) {
 
         String url="jdbc:mysql://localhost:3306/JDBC";
